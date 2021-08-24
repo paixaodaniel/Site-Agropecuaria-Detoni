2a01f937a337d20a092ed98650c390d1d8fe3b04
Ld-Ventura <78915896+Ld-Ventura@users.noreply.github.com>
2021-08-24 17:59:13 -0300

    Correções
    Correções:
    
    -Campo assunto recebendo somente números;
    -Formulário funcional

--- Agropecuaria Detoni/assets/mail/contact_me.php
+++ Agropecuaria Detoni/assets/mail/contact_me.php
@@ -12,16 +12,16 @@ if(empty($_POST['name'])      ||
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$assunto = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 
-$to = 'thiago@xbrain.com.br';
+// Create the email and send the message
+$to = 'thiago@xbrain.com.br'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulário de contado:  $name";
-$email_body = "Você recebeu uma nova mensagem do seu site.\n\n"."Aqui vão os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
-$headers = "From: noreply@ag.com\n";
+$email_body = "Você recebeu uma nova mensagem do seu site.\n\n"."Aqui vão os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nAssunto: $assunto\n\nMensagem:\n$message";
+$headers = "From: noreply@xbrain.pt\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
-
